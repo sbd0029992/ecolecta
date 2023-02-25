fdc9ed0059a929455abb985b916e43868c93d093
Diego Emerson Salvatierra Berrios <sbd0029992@est.univalle.edu>
2023-02-25 18:58:16 -0400

    started project

---
+++ src/pages/404.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+export default function NotFoundPage() {
+  return (
+    <div>
+      <main>
+        <section className='bg-white'>
+          <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
+            Error Page
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
