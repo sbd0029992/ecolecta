67eb37f506cc32c528caffcc801937c0fe88379e
Diego Emerson Salvatierra Berrios <sbd0029992@est.univalle.edu>
2023-03-20 17:45:27 -0400

    style(recolectorCheck): responsive working

--- src/pages/recolectorCheck.js
+++ src/pages/recolectorCheck.js
@@ -4,11 +4,11 @@ import { FaCheckCircle } from 'react-icons/fa';
 
 function RecolectorCheck() {
   return (
-    <div className='h-screen  bg-black font-secondary'>
-      <div className='flex h-screen flex-col items-center justify-center gap-20'>
-        <div className='flex w-[90vw] flex-col gap-5 rounded-xl bg-gray-200 p-4'>
+    <div className='h-screen  bg-black '>
+      <div className='flex h-screen flex-col items-center justify-center gap-20 sm:flex-row sm:p-3 '>
+        <div className='flex w-[90vw] flex-col justify-center gap-5 rounded-xl bg-gray-200  p-4 sm:h-fit sm:w-[300px] sm:gap-10'>
           <div className='mb-5 flex'>
-            <div className='w-[50vw] self-center'>
+            <div className='w-[50vw]  self-center'>
               <h2 className='font-medium'>Usuario: </h2>
             </div>
             <div className='min-w-fit self-center'>
@@ -40,24 +40,25 @@ function RecolectorCheck() {
             </button>
           </div>
         </div>
-        <div className='flex w-[90vw] flex-col gap-5 rounded-xl bg-secondary p-4 '>
+        <div className='flex w-[90vw] flex-col justify-center gap-5 rounded-xl bg-secondary p-4 font-secondary sm:h-fit sm:w-[300px] '>
           <div className='flex justify-between'>
-            <button className='h-14 w-[38vw] rounded-xl bg-primary text-2xl font-semibold text-white'>
+            <button className='h-14 w-[38vw] rounded-xl bg-primary text-2xl font-semibold text-white sm:w-full '>
               Estado
             </button>
-            <button className='h-14 w-[38vw] rounded-xl bg-primary text-2xl font-semibold text-white'>
+            <div className='w-5'></div>
+            <button className='h-14 w-[38vw] rounded-xl bg-primary text-2xl font-semibold text-white sm:w-full'>
               Ubicacion
             </button>
           </div>
           <div className='flex flex-col gap-4'>
             <div className='items-start'>
-              <h1 className='font-normal text-white  '>
+              <h1 className='text-center font-secondary text-white sm:text-3xl sm:font-normal '>
                 Hora estimada de llegada
               </h1>
             </div>
             <div className='flex justify-center'>
               <input
-                className='h-14 w-16 text-center text-2xl'
+                className='h-14 w-16 text-center text-2xl sm:w-20'
                 type='number'
                 placeholder='HH'
               />
@@ -65,7 +66,7 @@ function RecolectorCheck() {
                 <h1 className='m text-white'>:</h1>
               </div>
               <input
-                className='h-14 w-16 text-center text-2xl'
+                className='h-14 w-16 text-center text-2xl sm:w-20'
                 type='number'
                 placeholder='MM'
               />
