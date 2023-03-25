31c565d05881d047b3749c989fd2d62d3fc9bff8
Diego Emerson Salvatierra Berrios <sbd0029992@est.univalle.edu>
2023-03-24 20:19:52 -0400

    style(recolectoQr): styles fixed

--- src/pages/recolectorQr.js
+++ src/pages/recolectorQr.js
@@ -6,10 +6,9 @@ import imaimport from '../../public/images/imagenfondo.jpg';
 function Recolector_Qr() {
   let nombre = 'Jhon Pool Magne Rojas ';
   return (
-    <div class='h-max bg-[#4D7C0F]'>
-      <div class='grid-cols-2 place-items-center md:flex md:place-content-center md:pt-[200px]'>
-        <div class='h-[200px] w-auto rounded-3xl bg-white pt-[15px] md:h-[450px] md:w-[350px]'>
-          <button class=' ml-[390px] mr-[25px] md:collapse'> ||| </button>
+    <div class='h-full min-h-screen bg-[#4D7C0F]'>
+      <div class='min-h-screen grid-cols-2 place-items-center items-center sm:items-center md:flex md:place-content-center lg:flex-row '>
+        <div class='m-auto h-[200px] w-[500px] rounded-3xl bg-white pt-[15px] md:m-0 md:h-[450px] md:w-[350px]'>
           <div class='flex h-auto w-auto place-content-center'>
             <label class=' text-xl'> Recolector :</label>
             <label class=' ml-[10px] text-xl'> {nombre}</label>
@@ -28,7 +27,7 @@ function Recolector_Qr() {
         <label class='mt-[20px] mb-[20px] grid h-auto w-auto place-content-center text-4xl text-white md:invisible'>
           QR
         </label>
-        <div class='grid h-auto w-auto place-content-center rounded-3xl bg-white pb-[20px] md:h-[450px] md:w-[350px]'>
+        <div class='m-auto grid h-auto w-[500px] place-content-center rounded-3xl bg-white pb-[20px] md:m-0 md:h-[450px] md:w-[350px]'>
           <label class=' text-xl'> Escanea el codigo QR</label>
           <Image
             class=' mt-[20px] h-[200px] w-[200px] '
