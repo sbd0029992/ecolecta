4127feb0991df7ddb4f40f670393134d25188929
poolextremo <poolextremolol@gmail.com>
2023-03-31 19:27:28 -0400

    style(pages): recolector qr, recuperar cuenta, dashboard, item user list

--- src/pages/recolectorQr.js
+++ src/pages/recolectorQr.js
@@ -6,41 +6,51 @@ import imaimport from '../../public/images/imagenfondo.jpg';
 function Recolector_Qr() {
   let nombre = 'Jhon Pool Magne Rojas ';
   return (
-    <div class='h-full min-h-screen bg-[#4D7C0F]'>
-      <div class='min-h-screen grid-cols-2 place-items-center items-center sm:items-center md:flex md:place-content-center lg:flex-row '>
-        <div class='m-auto h-[200px] w-[500px] rounded-3xl bg-white pt-[15px] md:m-0 md:h-[450px] md:w-[350px]'>
-          <div class='flex h-auto w-auto place-content-center'>
-            <label class=' text-xl'> Recolector :</label>
-            <label class=' ml-[10px] text-xl'> {nombre}</label>
+    <div class='h-full min-h-[70vh] bg-secondary pt-5 pb-5 md:pt-0 md:pb-0'>
+      <div class='min-h-[70vh] grid-cols-2 place-items-center items-center sm:items-center md:flex md:place-content-center lg:flex-row '>
+        <div class='m-auto h-[150px] w-auto rounded-3xl bg-white pt-[15px] md:m-0 md:h-[350px] md:w-[400px]'>
+          <div class='flex h-auto w-auto flex-row place-content-center gap-2 '>
+            <div>
+              <label class=' text-lg'> Recolector:</label>
+            </div>
+            <div>
+              <label class=' text-lg'> {nombre}</label>
+            </div>
           </div>
           <div class='flex h-[0px] w-[0px] place-content-center md:w-auto'>
             <label class='invisible md:visible'>Placa: 2005AKS</label>
           </div>
           <div class='flex h-auto w-auto place-content-center'>
             <Image
-              class='mt-[20px] h-[100px] w-[100px] rounded-full md:mt-[50px] md:h-[150px] md:w-[150px]'
+              class='mt-[20px] h-[70px] w-[70px] rounded-full md:mt-[50px] md:h-[180px] md:w-[180px]'
               src={imaimport}
               alt='imagen de un check'
             />
           </div>
         </div>
-        <label class='mt-[20px] mb-[20px] grid h-auto w-auto place-content-center text-4xl text-white md:invisible'>
+        <label class='mt-[10px] mb-[10px] grid h-auto w-auto place-content-center text-4xl text-white md:invisible'>
           QR
         </label>
-        <div class='m-auto grid h-auto w-[500px] place-content-center rounded-3xl bg-white pb-[20px] md:m-0 md:h-[450px] md:w-[350px]'>
-          <label class=' text-xl'> Escanea el codigo QR</label>
-          <Image
-            class=' mt-[20px] h-[200px] w-[200px] '
-            src={imaimport}
-            alt='imagen de un check'
-          />
-          <button class=' mt-[20px] h-[50px] w-[200px] rounded-3xl bg-[#3bf027] text-xl text-white'>
-            {' '}
-          </button>
-          <button class=' mt-[20px] h-[50px] w-[200px] rounded-3xl bg-[#3bf027] text-xl text-white'>
-            {' '}
-            Comprobante{' '}
-          </button>
+        <div class='m-auto flex h-auto w-auto flex-col place-content-center gap-3 rounded-3xl bg-white p-5 pb-[20px] md:m-0 md:h-auto md:w-auto'>
+          <div class='flex flex-col items-center gap-2'>
+            <label class=' text-xl'> Escanea el codigo QR</label>
+          </div>
+          <div class='flex justify-center'>
+            <Image
+              class=' h-[200px] w-[200px] '
+              src={imaimport}
+              alt='imagen de un check'
+            />
+          </div>
+          <div class='flex flex-col items-center gap-2'>
+            <button class=' h-[30px] w-[200px] rounded-3xl bg-[#3bf027] text-xl text-white'>
+              {' '}
+            </button>
+            <button class=' h-[30px] w-[200px] rounded-3xl bg-[#3bf027] text-xl text-white'>
+              {' '}
+              Comprobante{' '}
+            </button>
+          </div>
         </div>
       </div>
     </div>
