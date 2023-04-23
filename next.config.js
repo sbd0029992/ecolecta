65f95e0e30aa5bce4aa4a7eb83274fd6b103c479
Diego Emerson Salvatierra Berrios <sbd0029992@est.univalle.edu>
2023-04-23 00:51:47 -0400

    fix(nextconfig): all working

--- next.config.js
+++ next.config.js
@@ -1,20 +1,23 @@
 /** @type {import('next').NextConfig} */
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-const nextConfig = {
+const nextConfig = withPWA({
   eslint: {
     dirs: ['src'],
   },
+  output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: [process.env.AWS_DOMAIN],
   },
-  // SVGR
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
@@ -32,18 +35,6 @@ const nextConfig = {
 
     return config;
   },
-};
+});
 
-if (isProduction) {
-  // Aplica next-pwa solo en producción
-  module.exports = withPWA({
-    ...nextConfig,
-    pwa: {
-      dest: 'public',
-      register: true,
-      skipWaiting: true,
-    },
-  });
-} else {
-  module.exports = nextConfig;
-}
+module.exports = nextConfig;
